3b809f851d8ff9a2afd306f707f5f73b938e28d9
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2019-01-11 11:45:44 +0200

    Minor cleanup

--- sv.py
+++ sv.py
@@ -8,9 +8,6 @@ import subprocess
 import sys
 import time
 
-import multiprocessing
-from multiprocessing import reduction
-
 import eventlet
 import eventlet.wsgi
 
@@ -18,7 +15,6 @@ from os_win import utilsfactory as os_win_utilsfactory
 
 # TODO: ditch pywin32
 import win32api
-import win32con
 import win32file
 import win32pipe
 import win32security
@@ -36,7 +32,6 @@ parser = argparse.ArgumentParser(
     description='Helper publishing subunit test results.')
 parser.add_argument('--pipe-handle', required=False)
 
-
 args = parser.parse_args()
 
 
@@ -53,24 +48,17 @@ def configure_logging(debug=True):
     LOG.addHandler(handler)
     LOG.setLevel(log_level)
 
+
 def app(env, start_response):
     LOG.info("Handling request.")
+    # time.sleep(10)
+    # import ctypes
+    # ctypes.windll.Kernel32.Sleep(10000)
     start_response('200 OK', [('Content-Type', 'text/plain')])
 
-    response = ['Test WSGI.\r\nWorker: %s' % os.getpid()]
+    response = ['Test WS.\r\nWorker: %s' % os.getpid()]
     LOG.info("Returning response: %s", response)
     return response
-
-def listen(addr, family=socket.AF_INET, backlog=50):
-    sock = socket.socket(family, socket.SOCK_STREAM)
-    if sys.platform[:3] != "win":
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    if hasattr(socket, 'SO_REUSEPORT'):
-        # NOTE(zhengwei): linux kernel >= 3.9
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    sock.bind(addr)
-    sock.listen(backlog)
-    return sock
 
 
 class Win32ProcessLauncher(object):
@@ -85,15 +73,6 @@ class Win32ProcessLauncher(object):
         LOG.info("Starting subprocess: %s", cmd)
 
         worker = subprocess.Popen(cmd, close_fds=False)
-        # import _subprocess
-        # _python_exe = os.path.join(sys.exec_prefix, 'python.exe')
-        # cmd = ' '.join('"%s"' % x for x in cmd)
-        # hp, ht, pid, tid = _subprocess.CreateProcess(
-        #         _python_exe, cmd, None, None, 1, 0, None, None, None
-        #         )
-        # ht.Close()
-        # import mock
-        # worker = mock.Mock(pid=pid)
         try:
             job_handle = self._processutils.kill_process_on_job_close(
                 worker.pid)
@@ -125,27 +104,16 @@ class Server(object):
         self._worker_count = worker_count
 
         self._launcher = Win32ProcessLauncher()
-        # import pdb; pdb.set_trace()
+
         if sock:
             self._sock = sock
-            # print(sock)
-            # print(dir(sock))
-            # self._sock = self._fromfd(sock.fileno())
         else:
             self._config_socket()
-
-    def _fromfd(self, fd, family=socket.AF_INET,
-                type_=socket.SOCK_STREAM, proto=0):
-        print(fd)
-        s = socket.fromfd(fd, family, type_, proto)
-        if s.__class__ is not socket.socket:
-            s = socket.socket(_sock=s)
-        return s
 
     def _config_socket(self, fromfd=None, family=socket.AF_INET,
                        type_=socket.SOCK_STREAM):
         addr = (BIND_ADDR, BIND_PORT)
-        self._sock = listen(addr, family)
+        self._sock = eventlet.listen(addr, family)
 
     def serve(self):
         eventlet.wsgi.server(self._sock, self._app, log=LOG,
@@ -169,9 +137,6 @@ class Server(object):
                     # Python 3 makes it easier to share sockets.
                     handle = None
                     try:
-                        # handle = win32api.OpenProcess(
-                        #     win32con.PROCESS_ALL_ACCESS, 0, worker.pid)
-                        # import pdb; pdb.set_trace()
                         share_sock_buff = self._sock.share(worker.pid)
                     finally:
                         if handle:
@@ -193,6 +158,7 @@ def create_pipe(sAttrs=-1, nSize=None):
         nSize = 0
 
     return win32pipe.CreatePipe(sAttrs, nSize)
+
 
 if __name__ == '__main__':
     configure_logging()
